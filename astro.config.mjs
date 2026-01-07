b61e7bf4cd6474ccfd5d25bbf679fcf7a14c89ad
Jordy756 <jordycastro1756@gmail.com>
2026-01-06 20:21:51 -0600

    feat(4G-123): update astro.config.mjs to include font providers and site URL chore(4G-123): modify package.json to open dev server by default refactor(4G-123): remove unused SVG assets from src/assets refactor(4G-123): delete Welcome component and update index.astro to use dialog refactor(4G-123): enhance Layout.astro with dynamic title and font integration chore(4G-123): update tsconfig.json to include path aliases for better imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,30 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://turrika.vercel.app",
+
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Unbounded",
+        cssVariable: "--font-unbounded",
+        styles: ["normal"],
+        weights: [300, 400, 500, 600, 700],
+        display: "swap",
+        subsets: ["latin"],
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Manrope",
+        cssVariable: "--font-manrope",
+        styles: ["normal"],
+        weights: [300, 400, 500, 600, 700],
+        display: "swap",
+        subsets: ["latin"],
+      },
+    ],
+  },
+});
